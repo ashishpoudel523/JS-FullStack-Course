f4685bf2a2cc4c1527b118d44e0d99c67cf43515
ashishpoudel523 <ashishpoudel523@gmail.com>
2026-03-28 23:50:58 +0545

    pern

--- P6-TodoApp/server/index.js
+++ P6-TodoApp/server/index.js
@@ -1,6 +1,17 @@
 import express from 'express'
+import cors from 'cors'
 const app = express()
 
+
+//middleware
+app.use(cors())
+app.use(express.json())
+
+app.get("/", (req, res) => {
+    res.send("Hi")
+})
+
+
 app.listen(5000, () => {
-    console.log("server started")
+    console.log(`server running on http://localhost:5000 `)
 })
